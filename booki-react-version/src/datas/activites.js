4ad7b9293b1c9bca405953e1be248101616146a2
Ale Raf <raffinalexis@gmail.com>
2023-04-13 15:33:11 +0200

    datas folder

---
+++ booki-react-version/src/datas/activites.js
@@ -0,0 +1,20 @@
+const activitesList = [
+    { 
+        title: "Vieux-Port", 
+        cover: require("../assets/images/activites/kevin-hikari-rV_Qd1l-VXg-unsplash.jpg")
+    },
+    { 
+        title: "Fort de Pomègues", 
+        cover: require("../assets/images/activites/lena-paulin-wH2-EJoDcV0-unsplash.jpg")
+    },
+    { 
+        title: "Parc national des Calanques", 
+        cover: require("../assets/images/activites/4_small/florian-wehde-xW9e8gdotxI-unsplash.jpg")
+    },
+    { 
+        title: "Notre-Dame-de-la-Garde", 
+        cover: require("../assets/images/activites/3_medium/paul-hermann-QFTrLdQIRhI-unsplash.jpg")
+    }  
+]
+
+export default activitesList
